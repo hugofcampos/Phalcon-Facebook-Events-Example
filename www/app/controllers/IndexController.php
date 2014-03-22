b0ee01369510caea926631aed145bcce57b45a41
Hugo Campos <hugofcampos@gmail.com>
2014-03-22 17:10:35 -0300

    Loading Facebook Events

--- www/app/controllers/IndexController.php
+++ www/app/controllers/IndexController.php
@@ -11,13 +11,18 @@ class IndexController extends \Phalcon\Mvc\Controller
     $config = array(
       'appId' => $this->config->facebookapp->appId,
       'secret' => $this->config->facebookapp->secret,
-      'allowSignedRequest' => false
+      'allowSignedRequest' => false,
+      'scope' => 'user_events',
     );
 
     $user = new User(new \Facebook($config));
 
     $this->view->setVar("user_profile", $user->getUser());
     $this->view->setVar("login_url", $user->getLoginUrl());
+    $this->view->setVar("events", $user->getEvents());
+    $this->view->setVar("has_permission", $user->hasPermission('user_events'));
+    $this->view->pick("index/index");
+
 
   }
 
--- www/app/libraries/Facebook/User.php
+++ www/app/libraries/Facebook/User.php
@@ -15,16 +15,19 @@ class User extends Injectable
 
   private $login_url;
 
+  private $permissions;
+
   public function __construct( \Facebook $facebook ){
     $this->facebook = $facebook;
     $this->user_id = $this->facebook->getUser();
-    $this->login_url = $this->getLoginUrl();
+    $this->login_url = $this->facebook->getLoginUrl(array('scope' => 'user_events'));
+    $this->permissions = $this->getPermissions();    
   }  
 
   public function getUser(){
     if($this->user_profile) return $this->user_profile;
     
-    if($this->user_id){    
+    if($this->user_id){
       try {
         $user_profile = $this->facebook->api('/me','GET');
         $this->user_profile = $user_profile;
@@ -42,6 +45,42 @@ class User extends Injectable
 
   public function getLoginUrl(){
     return $this->login_url;
+  }
+
+  public function getEvents(){    
+    if($this->user_id && $this->hasPermission('user_events')){
+      try {
+        $events = $this->facebook->api('/me/events', 'GET');
+        return $events;
+      
+      } catch(FacebookApiException $e) {
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }
+    }
+
+    return false;
+  }
+
+  public function getPermissions(){
+    if($this->permissions) return $this->permissions;
+
+    if($this->user_id){  
+      try {
+        $permissions = $this->facebook->api('/me/permissions', 'GET');
+        return $permissions;
+      
+      } catch(FacebookApiException $e) {
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }
+    }
+
+    return array();
+  }
+
+  public function hasPermission($permission){
+    return array_key_exists($permission, $this->permissions['data'][0]);
   }
 
 }
